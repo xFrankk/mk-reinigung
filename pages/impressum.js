4f29a6014c5ba47852afb929ab4d0ca25eada573
xFrankk <91114745+xFrankk@users.noreply.github.com>
2022-03-01 03:39:25 +0700

    Update impressum.js

--- pages/impressum.js
+++ pages/impressum.js
@@ -6,14 +6,8 @@ export default function Impressum() {
         className="py-20 px-4 sm:px-16 md:px-32 xl:px-40 2xl:px-96 text-center bg-green-100"
       >
         <h3 className="mb-10 sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
-          <b>Datenschutzerklärung</b>
+          <b>Impressum</b>
         </h3>
-        <h5 className="sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          <b>Allgemeiner Hinweis und Pflichtinformationen</b>
-        </h5>
-        <h5 className="sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          Die verantwortliche Stelle für die Datenverarbeitung auf dieser Website ist:
-        </h5>
         <h5 className="sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
           Mk-Reinigung
         </h5>
@@ -23,106 +17,55 @@ export default function Impressum() {
         <h5 className="sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
           86633 Neuburg/Donau
         </h5>
-        <h5 className="sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          Die verantwortliche Stelle entscheidet allein oder gemeinsam mit anderen über die Zwecke
-          und Mittel der Verarbeitung von personenbezogenen Daten (z.B. Namen, Kontaktdaten o. Ä.).
-        </h5>
         <h4 className="my-10 sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
-          <b>Widerruf Ihrer Einwilligung zur Datenverarbeitung</b>
+          <b>Urheberrecht</b>
         </h4>
         <h5 className="sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-         Nur mit Ihrer ausdrücklichen Einwilligung sind einige Vorgänge der Datenverarbeitung
-         möglich. Ein Widerruf Ihrer bereits erteilten Einwilligung ist jederzeit möglich. Für den
-         Widerruf genügt eine formlose Mitteilung per E-Mail. Die Rechtmäßigkeit der bis zum
-         Widerruf erfolgten Datenverarbeitung bleibt vom Widerruf unberührt.
+          Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
+          Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung,
+          Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+          Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des
+          jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
+          sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+          Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt
+          wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden
+          Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf
+          eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+          entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
+          werden wir derartige Inhalte umgehend entfernen.
         </h5>
         <h4 className="my-10 sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
-          <b>Recht auf Beschwerde bei der zuständigen Aufsichtsbehörde</b>
-          </h4>
+          <b>Haftung für Links</b>
         </h4>
         <h5 className="sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-         Als Betroffener steht Ihnen im Falle eines datenschutzrechtlichen Verstoßes ein
-         Beschwerderecht bei der zuständigen Aufsichtsbehörde zu. Zuständige Aufsichtsbehörde
-         bezüglich datenschutzrechtlicher Fragen ist der Landesdatenschutzbeauftragte des Bundeslandes,
-         in dem sich der Sitz unseres Unternehmens befindet.
-         Der folgende Link stellt eine Liste der Datenschutzbeauftragten
-         sowie deren Kontaktdaten bereit:
-            <a> https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html.</a>
+          Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren
+          Inhalte wir keinen Einfluss haben. Deshalb können wir für diese
+          fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
+          verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der
+          Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
+          Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige
+          Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine
+          permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne
+          konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
+          Bekanntwerden von Rechtsverletzungen werden wir derartige Links
+          umgehend entfernen.
         </h5>
         <h4 className="my-10 sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
-          <b>Recht auf Datenübertragbarkeit</b>
+          <b>Haftung für Inhalte</b>
         </h4>
         <h5 className="sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-         Ihnen steht das Recht zu, Daten, die wir auf Grundlage Ihrer Einwilligung oder in Erfüllung
-         eines Vertrags automatisiert verarbeiten, an sich oder an Dritte aushändigen zu lassen.
-         Die Bereitstellung erfolgt in einem maschinenlesbaren Format. Sofern Sie die direkte Übertragung
-         der Daten an einen anderen Verantwortlichen verlangen, erfolgt dies nur, soweit es technisch
-         machbar ist.
+          Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte
+          auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach
+          §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
+          verpflichtet, übermittelte oder gespeicherte fremde Informationen zu
+          überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
+          Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der
+          Nutzung von Informationen nach den allgemeinen Gesetzen bleiben
+          hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem
+          Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei
+          Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese
+          Inhalte umgehend entfernen.
         </h5>
-        <h4 className="my-10 sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
-          <b>Recht auf Auskunft, Berichtigung, Sperrung, Löschung</b>
-          </h4>
-          <h5 className="sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          Sie haben jederzeit im Rahmen der geltenden gesetzlichen Bestimmungen das Recht auf
-          unentgeltliche Auskunft über Ihre gespeicherten personenbezogenen Daten, Herkunft der Daten,
-          deren Empfänger und den Zweck der Datenverarbeitung und ggf. ein Recht auf Berichtigung,
-          Sperrung oder Löschung dieser Daten. Diesbezüglich und auch zu weiteren Fragen zum Thema
-          personenbezogene Daten können Sie sich jederzeit über die im Impressum aufgeführten
-          Kontaktmöglichkeiten an uns wenden.
-          </h5>
-          <h4 className="my-10 sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
-            <b>SSL- bzw. TLS-Verschlüsselung</b>
-            </h4>
-            <h5 className="sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          Aus Sicherheitsgründen und zum Schutz der Übertragung vertraulicher Inhalte,
-          die Sie an uns als Seitenbetreiber senden, nutzt unsere Website
-          eine SSL-bzw. TLS-Verschlüsselung. Damit sind Daten,
-          die Sie über diese Website übermitteln, für Dritte nicht mitlesbar.
-          Sie erkennen eine verschlüsselte Verbindung an der „https://“
-          Adresszeile Ihres Browsers und am Schloss-Symbol in der Browserzeile.
-            </h5>
-            <h4 className="my-10 sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
-              <b>Server-Log-Dateien</b>
-              </h4>
-              <h5 className="sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          In Server-Log-Dateien erhebt und speichert der Provider der Website automatisch Informationen,
-          die Ihr Browser automatisch an uns übermittelt. Dies sind:
-          - Besuchte Seite auf unserer Domain
-          - Datum und Uhrzeit der Serveranfrage
-          - Browsertyp und Browserversion
-          - Verwendetes Betriebssystem
-          - Referrer URL
-          - Hostname des zugreifenden Rechners
-          - IP-Adresse
-          Es findet keine Zusammenführung dieser Daten mit anderen Datenquellen statt.
-          Grundlage der Datenverarbeitung bildet Art. 6 Abs. 1 lit. b DSGVO, der die Verarbeitung
-           von Daten zur Erfüllung eines Vertrags oder vorvertraglicher Maßnahmen gestattet.
-           </h5>
-           <h4 className="my-10 sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl">
-             <b>Google Web Fonts</b>
-             </h4>
-          <h5 className="sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
-          Unsere Website verwendet Web Fonts von Google. Anbieter ist die Google Inc., 1600
-          Amphitheatre Parkway, Mountain View, CA 94043, USA.
-          Durch den Einsatz dieser Web Fonts wird es möglich Ihnen die von uns
-          gewünschte Darstellung unserer Website zu präsentieren, unabhängig davon welche Schriften
-         Ihnen lokal zur Verfügung stehen. Dies erfolgt über den Abruf der Google Web Fonts
-          von einem Server von Google in den USA und der damit verbundenen Weitergabe Ihre Daten an Google
-          . Dabei handelt es sich um Ihre IP-Adresse und welche Seite Sie bei uns besucht haben. Der Einsatz
-          von Google Web Fonts erfolgt auf Grundlage von Art. 6 Abs. 1 lit. f DSGVO. Als Betreiber dieser Website
-          haben wir ein berechtigtes Interesse an der optimalen Darstellung und Übertragung unseres Webauftritts.
-         Das Unternehmen Google ist für das us-europäische Datenschutzübereinkommen "Privacy Shield" zertifiziert.
-         Dieses Datenschutzübereinkommen soll die Einhaltung des in der EU geltenden Datenschutzniveaus gewährleisten.
-         Einzelheiten über Google Web Fonts finden Sie unter:
-         <a>https://www.google.com/fonts#AboutPlace:about</a>
-         und weitere Informationen in den Datenschutzbestimmungen von Google:
-         <a>https://policies.google.com/privacy/partners?hl=de</a>
-         Quelle: Datenschutz-Konfigurator von mein-datenschutzbeauftragter.de
-         </h5>
-
-
-
-
       </div>
     </>
   )
